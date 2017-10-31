6169e552c3a013e785353ad74ab3d310b500a00f
rmartinez96 <raul.martinez01@utrgv.edu>
2017-10-31 17:39:53 -0500

    Changed to work in Heroku.

---
+++ web.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+require 'data_mapper' # metagem, requires common plugins too.
+
+# need install dm-sqlite-adapter
+# if on heroku, use Postgres database
+# if not use sqlite3 database I gave you
+if ENV['DATABASE_URL']
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+else
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+end
+
+class Post
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+Post.auto_upgrade!
+
+get '/new' do
+	erb :new
+end
+
+post '/create' do
+	#replace this
+	p = Post.new
+	p.title = params[:title]
+	p.body = params[:body]
+	p.save
+	erb :success
+	#return params.to_s
+end
+
+get '/' do
+	#load all posts
+	#display them
+	@title = "All posts"
+	@headline = "My life in blog"
+	@paragraph = "Follow me plz. Follow 4 Follow."
+	@posts = Post.all
+
+
+	erb :index
+end
