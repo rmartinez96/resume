c544dc71c42e43a6f5a74d3bc7e611971c1b7094
rmartinez96 <raul.martinez01@utrgv.edu>
2017-10-31 23:50:52 -0500

    broken code

--- web.rb
+++ web.rb
@@ -1,4 +1,32 @@
 require 'sinatra'
+require 'pony'
+
+
+post '/contact' do 
+
+Pony.mail(
+	:name => params[:contactName],
+	:mail => params[:contactEmail],
+	:body => params[:contactMessage],
+	:to => 'raul.mtz2996@gmail.com',
+	:subject => params[:contactName] + " has contacted you",
+	:body => params[:contactMessage],
+	:port => '587',
+	:via => :smtp,
+	:via_options => { 
+	    :address              => 'smtp.gmail.com', 
+	    :port                 => '587', 
+	    :enable_starttls_auto => true, 
+	    :user_name            => 'raul.mtz2996', 
+	    :password             => 'Rm20317695', 
+	    :authentication       => :plain, 
+	    :domain               => 'localhost.localdomain'
+  })
+
+erb :index
+
+end
+
 
 get '/' do
 
