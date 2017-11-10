778893e1576a3dcb213bfe03d716f519334e172e
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2017-11-10 15:35:13 -0600

    Fix on index

--- web.rb
+++ web.rb
@@ -2,32 +2,6 @@ require 'sinatra'
 require 'pony'
 
 
-post '/create' do 
-
-Pony.mail(
-	:name => params[:contactName],
-	:mail => params[:contactEmail],
-	:body => params[:contactMessage],
-	:to => 'raul.mtz2996@gmail.com',
-	:subject => params[:contactName] + " has contacted you",
-	:port => '587',
-	:via => :smtp,
-	:via_options => { 
-	    :address              => 'smtp.gmail.com', 
-	    :port                 => '587', 
-	    :enable_starttls_auto => true, 
-	    :user_name            => 'raul.mtz2996', 
-	    :password             => 'Rm20317695', 
-	    :authentication       => :plain, 
-	    :domain               => 'localhost.localdomain'
-  })
-
-erb :index
-
-
-end
-
-
 get '/' do
 
 	erb :index
