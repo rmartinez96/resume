1206d04f579e09e1b825688bbc15a395cebdc0cf
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2017-11-10 15:31:51 -0600

    Almost done.

--- web.rb
+++ web.rb
@@ -1,31 +1,31 @@
 require 'sinatra'
-# require 'pony'
+require 'pony'
 
 
-# post '/contact' do 
+post '/create' do 
 
-# Pony.mail(
-# 	:name => params[:contactName],
-# 	:mail => params[:contactEmail],
-# 	:body => params[:contactMessage],
-# 	:to => 'raul.mtz2996@gmail.com',
-# 	:subject => params[:contactName] + " has contacted you",
-# 	:body => params[:contactMessage],
-# 	:port => '587',
-# 	:via => :smtp,
-# 	:via_options => { 
-# 	    :address              => 'smtp.gmail.com', 
-# 	    :port                 => '587', 
-# 	    :enable_starttls_auto => true, 
-# 	    :user_name            => 'raul.mtz2996', 
-# 	    :password             => 'Rm20317695', 
-# 	    :authentication       => :plain, 
-# 	    :domain               => 'localhost.localdomain'
-#   })
+Pony.mail(
+	:name => params[:contactName],
+	:mail => params[:contactEmail],
+	:body => params[:contactMessage],
+	:to => 'raul.mtz2996@gmail.com',
+	:subject => params[:contactName] + " has contacted you",
+	:port => '587',
+	:via => :smtp,
+	:via_options => { 
+	    :address              => 'smtp.gmail.com', 
+	    :port                 => '587', 
+	    :enable_starttls_auto => true, 
+	    :user_name            => 'raul.mtz2996', 
+	    :password             => 'Rm20317695', 
+	    :authentication       => :plain, 
+	    :domain               => 'localhost.localdomain'
+  })
 
-# erb :index
+erb :index
 
-# end
+
+end
 
 
 get '/' do
